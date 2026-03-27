bfb0f45f921f28cbfd60e7a113a98e252c09b150
hoangdat04 <165007922+hoangdat04@users.noreply.github.com>
2026-03-27 17:20:52 +0700

    Update TradingSystem.java

--- src/main/java/TradingSystem.java
+++ src/main/java/TradingSystem.java
@@ -1,21 +1,18 @@
 public class TradingSystem {
     public static final String ANSI_RED = "\u001B[31m";
     public static final String ANSI_RESET = "\u001B[0m";
-    
 	public static void main(String[] args) {
 	System.out.println("Hệ thống SMARS bắt đầu hoạt động...");
         // Giả lập server cũ nhận Flag=1 (nhẽ ra là lệnh mới nhưng code cũ hiểu là Power Peg)
         int receivedFlag = 1; 
         executeOrder(receivedFlag);
     }
-	
     public static void executeOrder(int flag) {
         if (flag == 1) {
         // ĐÂY LÀ MÃ NGUỒN CHẾT (DEAD CODE) LẼ RA PHẢI XÓA TỪ 8 NĂM TRƯỚC
             activatePowerPegMode(); 
         }
     }
-	//hoangdat
     private static void activatePowerPegMode() {
         int orders = 0;
         while (true) {
